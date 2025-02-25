e14c4b600a0fc911ce3292cc143bc89244fc68fd
Eric Du <98242715+exdu1@users.noreply.github.com>
2025-02-25 20:15:47 +0000

    updated .gitignore and created .env files

---
+++ server/server.js
@@ -0,0 +1,8 @@
+// Import packages
+import express from 'express';
+import cors from 'cors';
+import axios from 'axios';
+import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import { direname, join } from 'path';
+import fs from 'fs';
